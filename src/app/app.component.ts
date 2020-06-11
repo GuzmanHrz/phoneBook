db9fd0de4c86984ace9ee03a732fc58ff04a7b36
Guzman Hernandez <guzmanhernandez90@gmail.com>
2020-06-11 16:56:23 +0100

    Init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+type Contact = {
+  name:             string;
+  email:            string;
+  phone:            string;
+  contacted: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+/* Ana, Guzman, Manu, Isma
+ */
+  export class AppComponent {
   title = 'phoneBook';
+  newContact : Contact = {name: "" ,  email: "" , phone: "",contacted: false};
+  contactList  : Array<Contact>  = [];
+  errors : Array<string> = [];
+
+  addContact( contact : Contact) : void   { //Ana
+ 
+    console.log(contact);
+
+  }
+
+  deleteContact( contact: Contact) :void { //Guzman
+
+  }
+
+  isContactrepeated( contact: Contact) :boolean { //Manu
+    return false;
+  }
+
+  markAscontacted( contact: Contact ) :void { //Isma
+
+    return 
+
+  }
+
+/* esconder ya contactados */ // Ana
+
+  nameLongerThanSix( name: string ) :boolean { // Guzman
+    return
+
+  }
+
+  deleteAll() :void { // Manu
+
+  }
+
+  markAllasContacted () :void { //Isma
+
+  }
+  
+  
+  /* Segunda Parte */
+
+  search( data: string){ //Ana
+
+  }
+
+  searchByName(name: string){
+
+  }
+
+  searchByEmail(email:  string){
+
+  }
+
+  edit( contact: Contact) :void{ //Guzman
+
+  }
+
+  saveInLocalStorage(contact: Contact) :void { //Guzman
+
+  }
+
 }
