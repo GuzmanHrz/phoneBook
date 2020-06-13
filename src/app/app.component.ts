1ca645f32e7534ec5dc0d364c00185abb427769d
unknown <ana_v_r97@hotmail.com>
2020-06-13 20:30:11 +0100

    Pulling form master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 type Contact = {
+  id:               number;
   name:             string;
   email:            string;
   phone:            string;
@@ -12,25 +13,25 @@ type Contact = {
   styleUrls: ['./app.component.css']
 })
 
+
+
 /* Ana, Guzman, Manu, Isma
  */
   export class AppComponent {
   title = 'phoneBook';
-  newContact : Contact = {name: "" ,  email: "" , phone: "",contacted: false};
+  newContact : Contact = {id: 0, name: "" ,  email: "" , phone: "",contacted: false};
   contactList  : Array<Contact>  = [
-    {name: "Isma" ,   email: "" , phone: "745253453",contacted: false},
-    {name: "Juan" ,   email: "" , phone: "245353245",contacted: false},
-    {name: "Angel" ,  email: "" , phone: "324543534",contacted: false}
+    {id: 1, name: "Isma" ,   email: "" , phone: "745253453",contacted: false},
+    {id: 2, name: "Juan" ,   email: "" , phone: "245353245",contacted: true},
+    {id: 3, name: "Angel" ,  email: "" , phone: "324543534",contacted: false}
   ];
   errors : Array<string> = [];
   editMode: boolean = false;
   addMode: boolean = false;
+  idCounter: number = 3;
+
 
   addContact( contact : Contact) : void   { //Ana
-<<<<<<< Updated upstream
- 
-    console.log(contact);
-=======
     if(!this.isContactRepeated(contact) || this.nameLongerThanSix() ){
        contact.id = ++this.idCounter;
 
@@ -40,34 +41,37 @@ type Contact = {
     }
     
     console.log(this.contactList);
->>>>>>> Stashed changes
 
   }
 
   deleteContact( contact: Contact) :void { //Guzman
 
+    this.contactList  = this.contactList.filter( listItem => listItem !== contact );
+
   }
 
-  isContactrepeated( contact: Contact) :boolean { //Manu
-    return false;
+  isContactRepeated ( contact: Contact) :boolean { 
+    return this.contactList.some(elementItem => elementItem.name == contact.name && elementItem.email == contact.email && elementItem.phone == contact.phone); //Manu
+     
   }
 
   /* Isma */
-  private isAlreadyContacted = ( isContacted: boolean, position: number ) => !isContacted ? this.contactList[position].contacted = true : this.contactList[position].contacted = false
+
+  /* 
+    Función que busca un contacto por nombre y si lo encuentra lo marca como contactado
+    @param string Nombre del contacto a buscar
+  */
+  markAscontacted = ( contactName: string ) :void => this.contactList.forEach((contact: Contact, position: number) => contact.name === contactName ? this.contactList[position].contacted = true : false )
   
-  markAscontacted = ( contactName: string ) :void => this.contactList.forEach((contact: Contact, position: number) => contact.name === contactName ? this.isAlreadyContacted(contact.contacted, position) : false )
-  markAllasContacted = () :void => this.contactList.forEach((contact: Contact, position: number) => !contact.contacted ? this.contactList[position].contacted = true : false )
+  /* 
+    Función que recorre la lista de contactos y los marca todos como contactados
+  */
+  markAllasContacted = () :void => this.contactList.forEach((contact: Contact, position: number) => this.contactList[position].contacted = true )
 
   /* esconder ya contactados */ // Ana
 
-<<<<<<< Updated upstream
-  nameLongerThanSix( name: string ) :boolean { // Guzman
-    return
-
-=======
   nameLongerThanSix() :boolean { // Guzman
     return this.newContact.name.length > 6;
->>>>>>> Stashed changes
   }
 
   deleteAll() :void { // Manu
@@ -77,7 +81,7 @@ type Contact = {
   /* Segunda Parte */
 
   search( data: string){ //Ana
-      console.log(data);
+    
   }
 
   searchByName(name: string){
@@ -88,7 +92,14 @@ type Contact = {
 
   }
 
-  edit( contact: Contact) :void{ //Guzman
+  edit( contact: Contact ) :void{ //Guzman
+    
+    let originalContanct = this.contactList.find( itemList  => itemList.id === contact.id); 
+    originalContanct.name = contact.name;
+    originalContanct.email  =  contact.email;
+    originalContanct.phone = contact.phone;
+    originalContanct.contacted = contact.contacted;
+
 
   }
 
@@ -96,4 +107,9 @@ type Contact = {
 
   }
 
+  setAddMode() :void {
+    this.addMode = true;
+    this.newContact = {id: 0, name: "" ,  email: "" , phone: "",contacted: false};
+
+  }
 }
